b0c7fedb467a49b186fd3f468f7771c0718248ce
emlop <emlopez88@gmail.com>
2017-02-24 13:07:20 -0500

    nestedloops

---
+++ nestedLoops/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 400);  // create canvas
+}
+ 
+function draw() {
+  background(220); // background color
+  strokeWeight(4); 
+  stroke(0);
+ // var x = 0 to start at point zero
+  for (var x = 0; x <= width; x += 50) { // when mouse moved rect move downward
+    for (var y = 0; y <= mouseY; y += 50) { //  
+    fill(random(150), 0, random(100)); // random color output
+    rect(x, y, 50, 50); // rectangle
+    }
+  }
+   
+ 
+   
+}
