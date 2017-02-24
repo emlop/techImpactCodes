4af40ce44e6afb0c7c2097872ba43652eaaba945
emlop <emlopez88@gmail.com>
2017-02-24 16:45:20 -0500

    Update sketch.js

--- functionParameterDefined/sketch.js
+++ functionParameterDefined/sketch.js
@@ -1,8 +1,8 @@
 
 function setup() {
-	createCanvas(600, 480);
+	createCanvas(600, 480); //canvas created
 }
-
+// background, no stroke defined
 function draw() {
 	background(200);
 	noStroke();
@@ -13,13 +13,13 @@ function draw() {
 }
 
 // created function for the "first house" which is the triangle
-function house(x1,y1,x2,y2,x3,y3) {
-	fill(125, 225, 35);
+function house(x1,y1,x2,y2,x3,y3) { 
+	fill(125, 225, 35); // fill color for triangle
 	triangle(230, 225, 258, 170, 286, 225); // the x,y1,x2,y2,x3,y3 the parameters are defined here
 	
 function house2(x,y,w,h){ // created function for "2nd house" which is the rect
   
 }
-	fill(155, 0, 200);
+	fill(155, 0, 200); // fill color for rectangle 
 	rect(230, 225, 56, 60); //  the parameters are defined here
 }
