936669e04a8aff87fcbb2d972a3de5546a0b1b64
emlop <emlopez88@gmail.com>
2017-02-24 12:45:22 -0500

    array example

---
+++ array/sketch.js
@@ -0,0 +1,15 @@
+
+// list of array 
+var nums = [100, 55, 88, 125]; // an array with 4 items, index 0-3
+var num = 100; // a variable
+
+function setup() {
+	createCanvas(400, 400);
+}
+
+function draw() {
+	background(155, 135, 23);
+	ellipse(100, 200, num, num); // width and height are drawn at the variable num
+	ellipse(200, 200, nums[3], nums[3]); //width and height are drawn at index 3 for a value of 96
+	ellipse(300, 300, nums[2], nums[2]); //width and height are drawn at index 2 for a value of 29
+}
