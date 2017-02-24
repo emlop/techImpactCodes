047c4c5eb582cc4d8ea11d3a7f600f84fcd36e0a
emlop <emlopez88@gmail.com>
2017-02-24 16:38:18 -0500

    Update sketch.js

--- Basic Draw/sketch.js
+++ Basic Draw/sketch.js
@@ -1,9 +1,9 @@
 function setup() {
-  createCanvas(400,400);
-  background(255);
-  noStroke();
-  fill(0);
-  ellipse(300, 50, 50);
+  createCanvas(400,400); //canvas created
+  background(255); // background set to white
+  noStroke(); // no stroke line 
+  fill(0); // no fill color
+  ellipse(300, 50, 50); // sizing of the ellipse
 
 }
 
