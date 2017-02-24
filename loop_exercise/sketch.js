cb1bcde3eb62e6f4aa3f9aeb5614fa7a935f3d82
emlop <emlopez88@gmail.com>
2017-02-24 16:51:48 -0500

    Update sketch.js

--- loop_exercise/sketch.js
+++ loop_exercise/sketch.js
@@ -1,5 +1,8 @@
+
+// created variables for x and yPos
 var x = 0;
 var yPos = 0; 
+// defined canvas size, noLoop and framerate
 function setup() {
   createCanvas(600, 600);
   noLoop();
@@ -8,9 +11,10 @@ function setup() {
 function draw() {
   background(204);
   x = x + 1;
-  if (x > width) {
+  if (x > width) { // if x which = 0 greater than width +1 and loops on x axis
     x = 0;
   }
+  // fill, noStroke ellipse push are all defined here
   fill(100, 200, 255 )
   noStroke();
   ellipse(x, 300, x, 400);
@@ -20,13 +24,13 @@ function draw() {
   fill('pink');
   ellipse(mouseX, 450, mouseX, 300);
   
-  yPos = yPos -1;
+  yPos = yPos -1; // if yPos is less than 0, - 1 position
   if (yPos < 0) {
     yPos = height;
     ellipse(0, yPos, width, yPos);
   }
 }
-function mousePressed() {
-  loop();
+function mousePressed() { // when mousePressed pink ellipse moves on the y axis
+  loop(); // blue ellipse loops continuously 
 }
 
