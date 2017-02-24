3e487d956b6b3ad3e482edaebe6c036787a8c516
emlop <emlopez88@gmail.com>
2017-02-24 16:59:40 -0500

    Update sketch.js

--- sketch_variables/sketch.js
+++ sketch_variables/sketch.js
@@ -1,14 +1,15 @@
+// canvas and background defined
 function setup() {
   createCanvas(600, 400);
   background(150, 150, 100);
 }
 
 function draw() {
- 
-  fill(200, 100, 250);
-  ellipse(mouseX, mouseY, 100, 100);
+
+  fill(200, 100, 250); // filled color of ellipse
+  ellipse(mouseX, mouseY, 100, 100); // created an ellipse
   
-  function mousePressed() {
+  function mousePressed() { // when mousePressed ellipse spreads across the canvas
 
   }
 }
