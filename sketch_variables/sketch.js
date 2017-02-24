40259a51f8e55b93be8a2deafe5656456cf3453c
emlop <emlopez88@gmail.com>
2017-02-24 12:36:16 -0500

    mousePresses

---
+++ sketch_variables/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(600, 400);
+  background(150, 150, 100);
+}
+
+function draw() {
+ 
+  fill(200, 100, 250);
+  ellipse(mouseX, mouseY, 100, 100);
+  
+  function mousePressed() {
+
+  }
+}
